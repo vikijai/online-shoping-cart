1f1bed301288cd1868f3d7b7df34e58678a020c0
vikijai <vigneshjai98@gmail.com>
2021-06-07 22:38:59 +0530

    firebase-cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,48 @@ import { useSelector } from "react-redux";
 import {Icon} from "semantic-ui-react";
 import "./Header.css"
 import { Link } from "react-router-dom";
+import firebase from "../firebase";
+import { useState } from "react";
 const Header = () => {
+  const [username,setuser]=useState('');
+  const [isLogin,setisLogin]=useState(false);
+  const [photo,setphoto]=useState();
   const cartitem=useSelector((state) => state.cart);
+  onsubmit=()=>{
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    console.log(error)
+  });
+  }
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+       // console.log("Loggin in");
+        setuser(user.displayName);
+        setisLogin(true);
+        setphoto(user.photoURL);
+       // console.log(user.displayName);
+      } else {
+       // console.log("Loggin out");
+      }
+    });
+ const onlogout=()=>{
+      firebase.auth().signOut().then(() => {
+        setisLogin(false);
+      }).catch((error) => {
+        // An error happened.
+      });
+    }
   return (  
       <div className="header"> 
         <div className="shopname">
@@ -14,9 +54,20 @@ const Header = () => {
         <div className="input">
           <input className="inputtext" type="text"/>
         </div>
-        <div className="login">
-          <p className="logintext">hello vignesh</p>
+        <div  className="login">
+          {
+            isLogin===false?
+            <>
+            <h5 onClick={onsubmit} className="logintext">Login</h5>
+            </>
+            :
+            <div className="loggedin">
+            <h5 className="username" onClick={onlogout}>{username}</h5>
+            <img alt={photo} class="ui avatar image" src={photo}/>
+             </div> 
+          }
         </div>
+      
         <Link to={`/cart`}>
           <div className="icon">
           <Icon className="shopping cart" /> 
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCIaYJaDJll1igA4IjcOq2uB75sjpvz0dk",
+    authDomain: "shopping-cart-69da9.firebaseapp.com",
+    projectId: "shopping-cart-69da9",
+    storageBucket: "shopping-cart-69da9.appspot.com",
+    messagingSenderId: "541259943458",
+    appId: "1:541259943458:web:9138d5c859a67eb0b4c47b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
